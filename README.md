bf137bf31658b9b334870685e3c06039935732fe
Geordie <david.kaytes@gmail.com>
2012-06-03 17:39:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Codecademy jQuery Hover/Shadow Lesson
---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+   
+   $('.hover').hover(
+       function(){
+           $(this).children('a').animate({
+                bottom: '10px'
+           });
+           $(this).children('div').animate({
+                top: '10px',
+                opacity: 0.2
+           });
+       },
+       function(){
+           $(this).children('a').animate({
+                bottom: 0
+           });
+           $(this).children('div').animate({
+                top: 0,
+                opacity: 0.8
+           });
+      });
+      
+      $('.shadow').hover(
+       function(){
+           $(this).children('a').animate({
+                bottom: '10px'
+           });
+           $(this).children('div').animate({
+                opacity: 0.2
+           });
+       },
+       function(){
+           $(this).children('a').animate({
+                bottom: 0
+           });
+           $(this).children('div').animate({
+                opacity: 0.8
+           });
+      });
+});
